f4326dab93be7c0a6c80d0cc638846c908ce18f3
Antonio Villavicencio <tonyvcj@gmail.com>
2020-09-20 12:50:21 -0500

    Create interfaces for services

---
+++ pet-clinic-data/src/main/java/com/javc/springpetclinic/services/OwnerService.java
@@ -0,0 +1,13 @@
+package com.javc.springpetclinic.services;
+
+import com.javc.springpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}

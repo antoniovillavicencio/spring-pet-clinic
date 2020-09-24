99f6df52a26d9f00df6179c4997f1dbf0323fb2c
Antonio Villavicencio <tonyvcj@gmail.com>
2020-09-24 09:14:09 -0500

    Fix broken links and add not implemented template

--- pet-clinic-web/src/main/java/com/javc/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/javc/springpetclinic/controllers/VetController.java
@@ -12,7 +12,7 @@ public class VetController {
 
     public VetController(VetService vetService) {this.vetService = vetService;}
 
-    @RequestMapping({"/vets", "vets/index.html", "vets/index"})
+    @RequestMapping({"/vets", "/vets/index.html", "/vets/index", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";

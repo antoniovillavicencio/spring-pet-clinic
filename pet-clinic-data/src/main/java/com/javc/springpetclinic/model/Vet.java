d7757bcef7049ed95e5550518b44e7ac37e9f2bf
Antonio Villavicencio <tonyvcj@gmail.com>
2020-09-28 13:15:52 -0500

    Add Lombok

--- pet-clinic-data/src/main/java/com/javc/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/javc/springpetclinic/model/Vet.java
@@ -1,5 +1,11 @@
 package com.javc.springpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
@@ -9,6 +15,11 @@ import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -21,9 +32,5 @@ public class Vet extends Person {
 
     public Set<Specialty> getSpecialties() {
         return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
     }
 }
